ba1661008809e88ebf87e3517c7f221593a95963
delray2 <96529251+delray2@users.noreply.github.com>
2023-08-29 01:34:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,141 @@
 let player;
 let currentTrack;
 let accessToken;
+let device_id;
 
-// Function to fetch access token from your backend
+// Fetch the access token from the backend
 async function fetchAccessToken() {
-  const response = await fetch('http://localhost:8888/getAccessToken'); // Replace with your backend endpoint
-  const data = await response.json();
-  accessToken = data.access_token;
+  try {
+    const response = await fetch('http://localhost:8888/getAccessToken');
+    const data = await response.json();
+    accessToken = data.access_token;
+    console.log('Access Token:', accessToken);
+  } catch (err) {
+    console.error('Error fetching access token:', err);
+  }
 }
 
-// Initialize Spotify Web Playback SDK
+// Initialize the Spotify Web Playback SDK
 window.onSpotifyWebPlaybackSDKReady = async () => {
-  await fetchAccessToken(); // Fetch the access token
+  await fetchAccessToken();
+
+  if (!accessToken) {
+    console.error('Access token is not available. Make sure to fetch it before initializing the player.');
+    return;
+  }
 
   player = new Spotify.Player({
     name: 'Spotify Web Player',
     getOAuthToken: cb => { cb(accessToken); }
   });
 
-  player.addListener('player_state_changed', state => {
-    currentTrack = state.track_window.current_track;
-    document.getElementById('track-name').innerText = currentTrack.name;
-    document.getElementById('artist-name').innerText = currentTrack.artists[0].name;
-    document.getElementById('cover-art').src = currentTrack.album.images[0].url;
+  // Error handling
+  player.addListener('initialization_error', ({ message }) => { console.error('Initialization Error:', message); });
+  player.addListener('authentication_error', ({ message }) => { console.error('Authentication Error:', message); });
+  player.addListener('account_error', ({ message }) => { console.error('Account Error:', message); });
+  player.addListener('playback_error', ({ message }) => { console.error('Playback Error:', message); });
+
+  // Playback status updates
+  player.addListener('player_state_changed', state => { 
+    console.log('Player State Changed:', state);
+    updateDisplay(state);
   });
 
-  player.connect();
-};
+  // Ready
+  player.addListener('ready', async ({ device_id }) => {
+    console.log('Ready with Device ID', device_id);
+    await setActiveDevice(device_id);
+  });
 
+  // Connect to the player!
+  await player.connect();
+}
+
+// Update the display with current track info and album artwork
+function updateDisplay(state) {
+  if (state && state.track_window && state.track_window.current_track) {
+    const { name, album, artists } = state.track_window.current_track;
+    document.getElementById('current-track').textContent = `${name} - ${artists[0].name}`;
+    document.getElementById('album-art').src = album.images[0].url;
+  }
+}
+
+// Search for tracks
 async function search() {
   const query = document.getElementById('query').value;
-  const response = await fetch(`https://api.spotify.com/v1/search?q=${query}&type=track`, {
+  const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${query}`, {
     headers: {
       'Authorization': `Bearer ${accessToken}`
     }
   });
   const data = await response.json();
-  const results = data.tracks.items;
-  const resultsDiv = document.getElementById('search-results');
-  resultsDiv.innerHTML = '';
-  results.forEach(track => {
+  displaySearchResults(data.tracks.items);
+}
+
+// Display search results
+function displaySearchResults(tracks) {
+  const searchResultsDiv = document.getElementById('search-results');
+  searchResultsDiv.innerHTML = '';
+  tracks.forEach(track => {
     const trackDiv = document.createElement('div');
     trackDiv.className = 'search-item';
-    trackDiv.innerText = `${track.name} - ${track.artists[0].name}`;
-    trackDiv.onclick = () => queueTrack(track.uri);
-    resultsDiv.appendChild(trackDiv);
+    trackDiv.innerHTML = track.name + ' - ' + track.artists[0].name;
+    trackDiv.onclick = () => play(track.uri);
+    searchResultsDiv.appendChild(trackDiv);
   });
 }
 
-function queueTrack(uri) {
-  player.queue(uri);
+// Play a specified track on the Web Playback SDK's active device.
+async function play(trackUri) {
+  try {
+    await fetch(`https://api.spotify.com/v1/me/player/play`, {
+      method: 'PUT',
+      body: JSON.stringify({ uris: [trackUri] }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+      },
+    });
+  } catch (err) {
+    console.error('Error playing track:', err);
+  }
 }
 
-function play() {
-  player.togglePlay();
+// Pause playback
+async function pause() {
+  await player.pause();
+  console.log('Paused playback');
 }
 
-function pause() {
-  player.togglePlay();
+// Skip to the next track
+async function nextTrack() {
+  await player.nextTrack();
+  console.log('Skipped to next track');
 }
 
-function skip() {
-  player.nextTrack();
+// Skip to the previous track
+async function previousTrack() {
+  await player.previousTrack();
+  console.log('Moved to previous track');
 }
 
-function rewind() {
-  player.previousTrack();
+// Set the web player as the active device
+async function setActiveDevice(device_id) {
+  try {
+    await fetch(`https://api.spotify.com/v1/me/player`, {
+      method: 'PUT',
+      body: JSON.stringify({ device_ids: [device_id], play: false }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+      },
+    });
+  } catch (err) {
+    console.error('Error setting active device:', err);
+  }
 }
+
+// Initialize the Web Playback SDK upon page load
+window.addEventListener('load', () => {
+  fetchAccessToken();  // Fetch the access token right away
+});
